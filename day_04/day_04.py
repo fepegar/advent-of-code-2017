b38f2256346a57771a9ec3d0ab74185c238ba990
Fernando <fepegar@gmail.com>
2017-12-04 15:10:13 +0000

    Add second part of day 4

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -5,16 +5,21 @@ Solutions for day 4.
 from os.path import dirname, join
 
 
-def is_valid(passphrase):
+def is_valid(passphrase, anagram=False):
     split = passphrase.split()
     unique = set(split)
-    return len(split) == len(unique)
+    if not anagram:
+        return len(split) == len(unique)
+
+    sorted_words = [''.join(sorted(word)) for word in split]
+    sorted_set = set(sorted_words)
+    return len(sorted_words) == len(sorted_set)
 
 
-def valid_passphrases(passphrases):
+def valid_passphrases(passphrases, anagram=False):
     valid = 0
     for passphrase in passphrases:
-        if is_valid(passphrase):
+        if is_valid(passphrase, anagram=anagram):
             valid += 1
     return valid
 
@@ -33,6 +38,15 @@ def main():
     print('Solution to part 1: {}'.format(solution1))
 
     print()
+
+    print('Part 2')
+    EXAMPLES_2 = ['abcde fghij', 'abcde xyz ecdab', 'a ab abc abd abf abj',
+                  'iiii oiii ooii oooi oooo', 'oiii ioii iioi iiio']
+    for example in EXAMPLES_2:
+        solution = is_valid(example, anagram=True)
+        print('{} is valid: {}'.format(example, solution))
+    solution2 = valid_passphrases(passphrases, anagram=True)
+    print('Solution to part 2: {}'.format(solution2))
 
 
 
