9b828a56a2fb347ee1e148d162f84bc63a523c33
Fernando <fepegar@gmail.com>
2017-12-19 22:13:27 +0000

    Try to make part 2 of day 13 work

--- day_13/day_13.py
+++ day_13/day_13.py
@@ -7,7 +7,6 @@ from os.path import dirname, join
 
 
 
-
 class Layer:
 
     def __init__(self, depth, range_):
@@ -17,10 +16,27 @@ class Layer:
         self.DOWN = 1
         self.UP = -1
         self.direction = self.DOWN
+        self.packet = False
 
 
     def __repr__(self):
-        return '{}: {}'.format(self.depth, self.range)
+        strings = [str(self.depth).center(3)]
+        if self.range == 0:
+            if self.packet:
+                strings.append('(.)')
+            else:
+                strings.append('...')
+        for n in range(self.range):
+            if n == 0 and self.packet:
+                brackets = '(', ')'
+            else:
+                brackets = '[', ']'
+            if n == self.scanner:
+                inside = 'S'
+            else:
+                inside = ' '
+            strings.append(brackets[0] + inside + brackets[1])
+        return ' '.join(strings)
 
 
     def update(self):
@@ -39,6 +55,7 @@ class Layer:
     def reset(self):
         self.scanner = 0
         self.direction = self.DOWN
+        self.packet = False
 
 
 def read_firewall(input_path):
@@ -62,23 +79,62 @@ class Firewall:
         self.packet = -1
 
 
+    def __repr__(self):
+        strings = []
+        for layer in self.layers:
+            strings.append(repr(layer))
+        return '\n'.join(strings)
+
+
     def update(self):
         for layer in self.layers:
             layer.update()
 
 
-    def run(self):
+    def run(self, delay=None):
         severity = 0
+        caught = False
+
+        if delay is None:
+            delay = 0
+            return_if_caught = False
+        else:
+            return_if_caught = True
+
+        for _ in range(delay):
+            self.update()
+
+        # print(self)
+        current_layer = None
         for _ in range(len(self.layers)):
+            if current_layer is not None:
+                current_layer.packet = False
             self.packet += 1
             current_layer = self.layers[self.packet]
+            current_layer.packet = True
             caught = current_layer.scanner == 0
             if caught:
-                severity += current_layer.packet * current_layer.range
-            for layer in self.layers:
-                layer.update()
+                # print('caught!')
+                if return_if_caught:
+                    self.reset()
+                    return caught, severity
+                severity += current_layer.depth * current_layer.range
+            # print()
+            # print(self)
+            self.update()
         self.reset()
-        return severity
+        print('not caught!')
+        return caught, severity
+
+
+    def get_min_delay(self):
+        caught = True
+        delay = -1
+        while caught:
+            print('Try delay', delay+1)
+            delay += 1
+            caught = self.run(delay=delay)[0]
+        return delay
 
 
     def reset(self):
@@ -93,8 +149,12 @@ def main():
     firewall = Firewall(join(dirname(__file__), 'input.txt'))
 
     print('Part 1')
-    print('Solution to example: {}'.format(example.run()))
-    print('Solution to part 1: {}'.format(firewall.run()))
+    # print('Solution to example: {}'.format(example.run()[1]))
+    # print('Solution to part 1: {}'.format(firewall.run()[1]))
+
+    print('Part 2')
+    print('Solution to example: {}'.format(example.get_min_delay()))
+    print('Solution to part 2: {}'.format(firewall.get_min_delay()))
 
 
 if __name__ == '__main__':
