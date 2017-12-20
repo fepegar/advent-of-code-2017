0651babb802ccb9ff6fc2ad01bf7eed296a399c9
Fernando <fepegar@gmail.com>
2017-12-20 01:09:29 +0000

    Add second part of day 16

--- day_16/day_16.py
+++ day_16/day_16.py
@@ -16,12 +16,13 @@ def read_moves(input_path):
 
 class Programs:
     def __init__(self, length, moves):
-        self.programs = self.get_programs(length)
+        self.length = length
+        self.programs = self.get_programs()
         self.moves = moves
 
-    def get_programs(self, length):
+    def get_programs(self):
         a = ord('a')
-        return list(map(chr, range(a, a + length)))
+        return list(map(chr, range(a, a + self.length)))
 
     def dance_move(self, move):
         move_type = move[0]
@@ -49,7 +50,27 @@ class Programs:
     def dance(self):
         for move in self.moves:
             self.dance_move(move)
-        return ''.join(self.programs)
+        return self.beautify()
+
+    def long_dance(self, times=int(1e9)):
+        record = []
+        step = 0
+        while True:
+            self.dance()
+            if self.programs in record:
+                period = step
+                break
+            else:
+                record.append(self.programs[:])
+                step += 1
+        index = times % period - 1
+        result = record[index]
+        return self.beautify(result)
+
+    def beautify(self, programs=None):
+        if programs is None:
+            programs = self.programs
+        return ''.join(programs)
 
 
 def main():
@@ -61,6 +82,14 @@ def main():
     print('Solution to example: {}'.format(example.dance()))
     programs = Programs(16, moves)
     print('Solution to part 1: {}'.format(programs.dance()))
+
+    print()
+
+    print('Part 2')
+    example = Programs(5, EXAMPLE_1)
+    print('Solution to example: {}'.format(example.long_dance()))
+    programs = Programs(16, moves)
+    print('Solution to part 1: {}'.format(programs.long_dance()))
 
 
 if __name__ == '__main__':
