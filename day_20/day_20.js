d8e18ecb0a7a919f6533cb3ed0bf98f0a6d61bff
Fernando <fepegar@gmail.com>
2017-12-22 17:58:42 +0000

    Add second part of day 20

--- day_20/day_20.js
+++ day_20/day_20.js
@@ -3,6 +3,7 @@
  */
 
 var particles;
+var part = 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,12 +21,20 @@ function draw() {
   fill(255, 50);
   ellipse(0, 0, 20);
 
+  if (part == 2) {
+    collide();
+  }
+
   for (var i = 0; i < particles.length; i++) {
     particles[i].update();
     particles[i].draw();
   }
   var closest = getClosestToCenter();
-  console.log("Closest:", closest)//, particles[closest].p.z);
+  if (part == 1) {
+    console.log("Closest:", closest);
+  } else if (part == 2) {
+    console.log("Particles:", particles.length);
+  }
 }
 
 
@@ -41,4 +50,27 @@ function getClosestToCenter() {
     }
   }
   return closest;
+}
+
+
+function collide() {
+  var destroy = new Set();
+  var particle1;
+  var particle2;
+  for (var i = 0; i < particles.length - 1; i++) {
+    particle1 = particles[i];
+    for (var j = i+1; j < particles.length; j++) {
+      particle2 = particles[j];
+      if (particle1.p.dist(particle2.p) < 0.0001) {
+        destroy.add(particle1);
+        destroy.add(particle2);
+      }
+    }
+  }
+
+  var index;
+  for (let particle of destroy) {
+    index = particles.indexOf(particle);
+    particles.splice(index, 1);
+  }
 }
