d84fa0201fa71c2f33f41dea452791ef211d5f12
Fernando <fepegar@gmail.com>
2017-12-26 01:38:49 +0000

    Remove do-whiles

--- day_23/day_23_2.c
+++ day_23/day_23_2.c
@@ -2,7 +2,7 @@
 
 int main() {
   int a = 1;
-  int b, c, d, e, f;
+  int b, c, i, j, f;
   int h = 0;
 
   b = 67;  // 1 set b 67
@@ -12,27 +12,27 @@ int main() {
   c = b;  // 7 set c b
   c += 17000;  // 8 sub c -17000
 
-  do {
+  while (1) {
     f = 1;  // 9 set f 1
-    d = 2;  // 10 set d 2
-    do {
-      e = 2;  // 11 set e 2
-      do {
-        if ((d * e) == b) {
+
+    for (int i = 2; i < b; i++) {
+      for (int j = 2; j < b; j++) {
+        if ((i * j) == b) {
           f = 0;  // 16 set f 0
         }
-        e += 1;  // 17 sub e -1
-      } while(e != b);
-      d += 1;  // 21 sub d -1
-    } while(d != b);
+      }
+    }
+
     if (f == 0) {  // 25 jnz f 2
       h += 1;  // 26 sub h -1
     }
+
     if (b == c) {
       break;  // 30 jnz 1 3
     }
+
     b += 17;  // 31 sub b -17
-  } while(1);  // 32 jnz 1 -23
+  }
 
   printf("h: %d\n", h);
   return 0;
